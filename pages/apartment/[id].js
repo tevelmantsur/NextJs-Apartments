256c75474a8f64d7fb069400046a1df82d760658
tevel mantusr <tevelmantsur@gmail.com>
2022-03-31 19:48:55 +0300

    update [id]

--- pages/apartment/[id].js
+++ pages/apartment/[id].js
@@ -15,7 +15,7 @@ export async function getStaticPaths(props) {
   console.log(props);
   return {
     // Only `/posts/1` and `/posts/2` are generated at build time
-    paths: [{ params: { id: params.id } }],
+    paths: [{ params: { id: props.params.id } }],
     // Enable statically generating additional pages
     // For example: `/posts/3`
     fallback: true,
