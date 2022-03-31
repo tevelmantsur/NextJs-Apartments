6bd7b7dde99a577bb14e7910ed7749e4c6aa8a83
tevel mantusr <tevelmantsur@gmail.com>
2022-03-31 20:24:10 +0300

    update [id]

--- pages/apartment/[id].js
+++ pages/apartment/[id].js
@@ -1,5 +1,3 @@
-/** 
-
 import * as React from "react";
 import { useRouter } from "next/router";
 
@@ -12,33 +10,3 @@ export default function NavBar({ data }) {
 
   return <>{!data ? "loading" : <h1>{data.id}</h1>}</>;
 }
-
-export async function getStaticPaths(props) {
-  console.log(props);
-  return {
-    // Only `/posts/1` and `/posts/2` are generated at build time
-    paths: [{ params: { id: "py0h2c8x" } }],
-    // Enable statically generating additional pages
-    // For example: `/posts/3`
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  console.log(params);
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(
-    `https://express-database.vercel.app/apartment/${params.id}`
-  );
-  const data = await res.json();
-
-  // Pass post data to the page via props
-  return {
-    props: { data },
-    // Re-generate the post at most once per second
-    // if a request comes in
-    revalidate: 1,
-  };
-}
-**/
