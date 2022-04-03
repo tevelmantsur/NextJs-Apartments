e74b530399b7004a4a026a2380522b24b0711158
tevel mantusr <tevelmantsur@gmail.com>
2022-04-03 21:15:21 +0300

    new item

--- pages/api/auth/lib/mongodb.js
+++ pages/api/auth/lib/mongodb.js
@@ -10,7 +10,7 @@ const options = {
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
