8b1692fead9051005f1b0452a27b80910e76dfe5
tevel mantusr <tevelmantsur@gmail.com>
2022-04-05 01:33:50 +0300

    RDD test remove

--- pages/apartment/new.js
+++ pages/apartment/new.js
@@ -13,7 +13,6 @@ import NavBar from "../../components/navBar";
 import { isMobile } from "react-device-detect";
 import * as rdd from "react-device-detect";
 export default function Search({ data, query }) {
-  rdd.isMobile = true;
   const [Query, setQuery] = useState(query);
   const [drawer, setDrawer] = useState({ drawerOpen: false, name: "פתח" });
   console.log(query);
